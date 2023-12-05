1310c5a67ac936569efe9d2942474d2081ab6d89
Alex Moran <ebs.alex@icloud.com>
2023-12-04 22:32:41 -0800

    crated modal dailog for adding book entries and styled it.

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let title = document.querySelector("h1")
+
+let title = document.querySelector("h1");
+let addBookDialog = document.querySelector("#addBookDialog")
+let addBtn = document.querySelector(".addBtn");
+addBtn.addEventListener("click", () => addBookDialog.showModal())
 
 
 
@@ -11,25 +15,40 @@ let title = document.querySelector("h1")
 
 
 
+const myLibrary = [];
+
+function Book(title, author, pages, description, readStatus, submittedBy) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.description = description;
+  this.readStatus = readStatus;
+  this.submittedBy = submittedBy
+}
+
+
+
+function addBookToLibrary() {
+    // do stuff here
+}
 
 
 
 
 
 
+let readCheckboxLabel = document.querySelector("label[for='readCheckbox']");
+let readCheckbox = document.querySelector("#readCheckbox")
+readCheckbox.addEventListener("change", () => {
+    if (readCheckbox.checked) {
+        readCheckboxLabel.textContent = "Book has been read"
+    } else {
+        readCheckboxLabel.textContent = "Book is unread"
+    }
+})
 
 
-
-
-
-
-
-
-
-
-
-
-///Example cards 
+//////Example cards 
 
 let example1 = document.querySelector("#example1")
 let readStatus1 = document.querySelector("#readStatus1")
@@ -39,13 +58,17 @@ let rmvBtn1 = document.querySelector("#rmvBtn1")
 readCheckbox1.addEventListener("change", () => {
     if (readCheckbox1.checked) {
         readStatus1.textContent = "Read"
+        readStatus1.style.color = "black";
     } else {
         readStatus1.textContent = "Unread"
+        readStatus1.style.color = "bisque";
     }
 })
 
 rmvBtn1.addEventListener("click", () => {
-    example1.style.display = "none"
+    if (confirm("Are you sure?")) {
+        example1.style.display = "none"
+    };
 })
 
 
@@ -57,26 +80,34 @@ let rmvBtn2 = document.querySelector("#rmvBtn2")
 readCheckbox2.addEventListener("change", () => {
     if (readCheckbox2.checked) {
         readStatus2.textContent = "Read"
+        readStatus2.style.color = "black";
     } else {
         readStatus2.textContent = "Unread"
+        readStatus2.style.color = "bisque";
     }
 })
 
 rmvBtn2.addEventListener("click", () => {
-    example2.style.display = "none"
+    if (confirm("Are you sure?")) {
+        example2.style.display = "none"
+    };
 })
 
 
 
+//////run at least once at init
 
-
-// readStatus1.textContent = "Read"
-
-
-
-// exampleCards.forEach(addEventListener)
-
-
-
-
-// title.style.textDecoration = "underline"
+if (readCheckbox1.checked) {
+    readStatus1.textContent = "Read"
+    readStatus1.style.color = "black";
+} else {
+    readStatus1.textContent = "Unread"
+    readStatus1.style.color = "bisque";
+}
+if (readCheckbox2.checked) {
+    readStatus2.textContent = "Read"
+    readStatus2.style.color = "black";
+} else {
+    readStatus2.textContent = "Unread"
+    readStatus2.style.color = "bisque";
+}
